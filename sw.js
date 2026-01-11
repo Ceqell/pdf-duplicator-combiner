5e1a6bfe4a5fd2a65da49860f0e85c3e8f7a678a
Ceqell <102220034+Ceqell@users.noreply.github.com>
2026-01-11 19:54:31 +0700

    Update versioning

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version string whenever you update styles or JS!
-const CACHE_NAME = 'pdf-duplicator-v1.1.2-1';
+const CACHE_NAME = 'pdf-duplicator-v1.1.2-10';
 
 const ASSETS = [
   './',
