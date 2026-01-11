7ede5693aacc6662bb2b6870e6ba698bc8b81e64
Ceqell <102220034+Ceqell@users.noreply.github.com>
2026-01-11 20:12:58 +0700

    Style

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version string whenever you update styles or JS!
-const CACHE_NAME = 'pdf-duplicator-v1.1.2-11';
+const CACHE_NAME = 'pdf-duplicator-v1.1.2-12';
 
 const ASSETS = [
   './',
