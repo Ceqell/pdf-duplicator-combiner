74535df3c41ce47f36a737fadc3e6a24ed3d36ab
Ceqell <102220034+Ceqell@users.noreply.github.com>
2026-01-12 11:21:40 +0700

    Update icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change this version string whenever you update styles or JS!
-const CACHE_NAME = 'pdf-duplicator-v1.1.2-15';
+const CACHE_NAME = 'pdf-duplicator-v1.1.2-16';
 
 const ASSETS = [
   './',
